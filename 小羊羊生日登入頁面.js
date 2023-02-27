cb4f8a4a7fbb494776adff460d4a773f9c6e19ef
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-02-27 15:10:37 +0800

    Update 小羊羊生日登入頁面.js

--- 小羊羊生日登入頁面.js
+++ 小羊羊生日登入頁面.js
@@ -1,6 +1,8 @@
 function CheckPassword(Password){
         if(Password != "15yearsbirthday"){
-        location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E5%AF%86%E7%A2%BC%E8%BC%B8%E5%85%A5%E9%8C%AF%E8%AA%A4.html")}
+        location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E5%AF%86%E7%A2%BC%E8%BC%B8%E5%85%A5%E9%8C%AF%E8%AA%A4.html");
+        }
         else{
-        location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E7%94%9F%E6%97%A5%E5%BD%B1%E7%89%87.html")}
+        location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E7%94%9F%E6%97%A5%E5%BD%B1%E7%89%87.html");
+        }
 }
