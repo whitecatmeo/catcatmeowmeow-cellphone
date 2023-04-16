1b0c33213d67900c2f6916f9637383f62705d529
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-04-16 20:14:01 +0800

    Update 總連結4.js

--- 總連結4.js
+++ 總連結4.js
@@ -23,7 +23,7 @@ function bb() {
     location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E7%94%9F%E6%97%A5%E5%BD%B1%E7%89%878.html")
 }
 function cc() {
-    location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E7%B5%A6%E5%B0%8F%E7%BE%8A%E7%BE%8A%E7%9A%84%E8%A9%B1.html")
+    location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E5%91%8A%E7%99%BD%E6%96%87")
 }
 function mm() {
     location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/ee/%E6%9C%80%E5%BE%8C%E4%B8%80%E9%A0%81.html")
