0fa5c34f19f7959c1aaec4a4755db65e35b23bf5
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-04-16 20:18:31 +0800

    Update 生日影片8.js

--- 生日影片8.js
+++ 生日影片8.js
@@ -2,7 +2,7 @@ function aa() {
     location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E7%94%9F%E6%97%A5%E5%BD%B1%E7%89%877.html")
 }
 function cc() {
-    location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E7%B5%A6%E5%B0%8F%E7%BE%8A%E7%BE%8A%E7%9A%84%E8%A9%B1.html")
+    location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E5%91%8A%E7%99%BD%E6%96%87")
 }
 /*
 aa生日影片7
