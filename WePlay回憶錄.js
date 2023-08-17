bb6fbc87c081a4890e3bbaf2a4ee8fb4520294e7
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-08-17 15:51:29 +0800

    Add files via upload

---
+++ WePlay回憶錄.js
@@ -0,0 +1,3 @@
+function photoandmp4() {
+    location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/%E5%81%B7%E6%8B%8D%E5%81%B7%E9%8C%84and%E5%81%B7%E5%AD%98")
+}
---
+++ 偷拍and偷錄回憶錄.js
@@ -0,0 +1,3 @@
+function weplay() {
+    location.href=("https://whitecatmeo.github.io/catcatmeowmeow-cellphone/WePlay%E5%9B%9E%E6%86%B6%E9%8C%84")
+}
